a43a6b4021ba322c586f88d669eb57e2ada74d4c
raiyanmrf <raiyanmrf@gmail.com>
2024-01-07 18:43:27 +0600

    "js footer"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,25 @@ const Footer = () => {
   return (
     <section className='p-2 relative flex gap-3  text-white justify-between  bg-red-600 h-[400px] w-full'>
       <div className='flex flex-col'>
-        <h3 className='text-xl px-3 '>20% off </h3>
+        <h3 className='text-xl px-3 font-bold '>20% off </h3>
         <h1 className='text-[160px] relative sm:bottom-10 max-sm:text-[110px] font-bold text-white'>
           FINE
         </h1>
         <h1 className='text-[160px]  relative max-sm:bottom-[50px] sm:bottom-[120px] max-sm:text-[100px] font-bold text-white'>
           SMILE
         </h1>
-        <h3 className='text-xl px-3 relative sm:bottom-[150px]'>
-          15 Nov to 17 Dec
+        <h3 className='text-2xl sm:hidden px-3 relative font-bold sm:bottom-[150px]'>
+          Summer Sale
         </h3>
       </div>
 
       <div className='flex flex-col gap-10'>
         <p className='text-2xl relative font-bold '>Beats Solo Air </p>
-        <h2 className='text-6xl max-sm:text-3xl max-sm:right-[50px] z-20 max-sm:whitespace-nowrap relative font-bold '>
+        <h2 className='text-6xl max-sm:hidden relative font-bold '>
           Summer Sale
         </h2>
         <button
-          className=' right-1  bg-white w-[150px] 
+          className=' right-2 top-10  bg-white w-[150px] 
       max-sm:w-[130px] max-sm:text-xl  py-2 text-2xl text-red-600 font-bold
        rounded-xl relative z-20'
         >
