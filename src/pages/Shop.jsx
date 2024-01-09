b84ea6c992dd80e3da9846c271aaacf6f793d002
raiyanmrf <raiyanmrf@gmail.com>
2024-01-09 19:09:48 +0600

    'm'

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -11,45 +11,74 @@ import {
 } from "react-icons/ai";
 
 const Shop = () => {
-  const { cartItems, addTocart } = useContext(Shopcontext);
+  const { cartItems, addTocart, removeFromcart, updateCartItem } =
+    useContext(Shopcontext);
   const [bigImage, setbigImage] = useState(banner);
+  const [smallImages, setSmallimages] = useState("hpbanner");
+
   return (
     <main className='relative top-[80px] mx-auto p-1 '>
-      <section className='flex justify-between'>
-        <div>
+      <section className='flex relative top-0 justify-center items-center gap-2 sm:gap-40'>
+        <div className=' '>
           <div>
-            <img src={bigImage} alt='bigImage' />
+            <img width={"500px"} src={bigImage} alt='bigImage' />
           </div>
-          <div className='flex  rounded-lg hover:animate-pulse '>
-            <img src={banner} width={"100px"} alt='smallImages' />
-            <img src={banner} width={"100px"} alt='smallImages' />
-            <img src={banner} width={"100px"} alt='smallImages' />
-            <img src={banner} width={"100px"} alt='smallImages' />
-          </div>
+          <div className='flex gap-2 max-sm:absolute bottom-[-130px] justify-center items-center'>
+            {PRODUCTS &&
+              PRODUCTS.filter(p => p.tag === smallImages).map(p => {
+                return (
+                  <div key={p.id} className='flex rounded-lg bg-stone-300'>
+                    <img
+                      onMouseOver={() => setbigImage(p.productImage)}
+                      className={`hover:bg-red-600`}
+                      width={"100px"}
+                      src={p.productImage}
+                    />
+                  </div>
+                );
+              })}
+          </div>{" "}
         </div>
 
-        <article>
-          <h2>NAme</h2>
-          <div className='flex'>
-            <AiFillStar style={{ color: "red" }} />
-            <AiFillStar style={{ color: "red" }} />
-            <AiFillStar style={{ color: "red" }} />
-            <AiOutlineStar style={{ color: "red" }} />{" "}
-          </div>
-          <h3>Detail</h3>
-          <p>Desc</p>
-          <h3>Price</h3>
-          <h3 className='flex justify-center items-center'>
-            quantity:
-            <span className='border flex justify-center items-center'>
-              <AiOutlinePlus />
-              <input className='w-10 border' type='text' />
-              <AiOutlineMinus />
-            </span>
-          </h3>
+        <article className='sm:w-[50%]'>
+          {PRODUCTS &&
+            PRODUCTS.filter(p => p.productImage === bigImage).map(p => {
+              return (
+                <div key={p.id}>
+                  <h2>{p.productName}</h2>
+                  <div className='flex'>
+                    <AiFillStar style={{ color: "red" }} />
+                    <AiFillStar style={{ color: "red" }} />
+                    <AiFillStar style={{ color: "red" }} />
+                    <AiOutlineStar style={{ color: "red" }} />{" "}
+                  </div>
+                  <h3>Detail</h3>
+                  <p className='max-sm:text-[13px] sm:break-words max-w-sm'>
+                    {p.productDesc.slice(0, 190)}.
+                  </p>
+                  <h3>{p.price}$</h3>
+                  <h3 className='flex justify-start gap-1 items-start'>
+                    Quantity:
+                    <span className='border flex justify-center items-center'>
+                      <AiOutlinePlus onClick={() => addTocart(p.id)} />
+                      <input
+                        className='w-5 text-center border'
+                        type='text'
+                        value={cartItems[p.id]}
+                        onChange={e => {
+                          updateCartItem(Number(e.target.value), p.id);
+                        }}
+                      />
+                      <AiOutlineMinus onClick={() => removeFromcart(p.id)} />
+                    </span>
+                  </h3>
+                </div>
+              );
+            })}
         </article>
       </section>
-      <section className='mt-[120px]'>
+      <section className='mt-[190px]'>
+        <h1 className='text-4xl'>You May Also Like</h1>
         <div className='relative h-[400px] w-full  overflow-x-hidden'>
           <div className='flex justify-center gap-[15px] mt-[20px] absolute whitespace-nowrap track '>
             {PRODUCTS &&
@@ -63,7 +92,13 @@ const Shop = () => {
                       {product.productName}
                     </h3>
                     <p className='text-sm'>{product.price}$</p>
-                    <button onClick={() => addTocart(product.id)}>
+                    <button
+                      onClick={() => {
+                        setbigImage(product.productImage);
+                        addTocart(product.id);
+                        setSmallimages(product.tag);
+                      }}
+                    >
                       Add to cart{" "}
                       {cartItems[product.id] > 0 && (
                         <span>({cartItems[product.id]})</span>
